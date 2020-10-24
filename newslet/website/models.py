f4438d2bb76d314113fba56238d1c3cc2f867b61
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-10-24 16:56:48 +0200

    e-mail connected and tested

--- newslet/website/models.py
+++ newslet/website/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class TestModel(models.Model):
     title = models.CharField(max_length=100)
+
+
+class WisdomNote(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=120, default='Annonymous')
--- newslet/website/views.py
+++ newslet/website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 def home(request):
     context = {
         'name': "mike"
     }
+
     return render(request, 'website/home.html', context)
