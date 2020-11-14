a55cdbf9ea19dc698cd289629fb8cd77cca0b6e6
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-11-14 19:50:29 +0100

    login, logout, register done

--- newslet/website/views.py
+++ newslet/website/views.py
@@ -4,11 +4,7 @@ from django.core.mail import send_mail
 
 
 def home(request):
-    context = {
-        'name': "mike"
-    }
-
-    return render(request, 'website/home.html', context)
+    return render(request, 'website/home.html')
 
 
 def about(request):
