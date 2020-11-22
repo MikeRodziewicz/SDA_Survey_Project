2b83624a4b0a2d1a81c7ad7dfbbd264c14e0ece4
pZajac1506 <editruskawka@gmail.com>
2020-11-22 14:54:48 +0100

    wyświetl zmodyfikowany komunikat

--- newslet/website/admin.py
+++ newslet/website/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import GuestSurvey
+from .models import TestModel, WisdomNote, NewsTags, NewsCategory, Formularz
 
-admin.site.register(GuestSurvey)
+admin.site.register(TestModel)
+admin.site.register(WisdomNote)
+admin.site.register(NewsTags)
+admin.site.register(NewsCategory)
+admin.site.register(Formularz)
