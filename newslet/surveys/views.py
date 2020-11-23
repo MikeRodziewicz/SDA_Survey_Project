045f3b88161cab005fee4b697c4b31384cf3e16c
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-11-23 21:29:35 +0100

    email functionality working

--- newslet/surveys/views.py
+++ newslet/surveys/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from website.models import GuestSurvey
+from django.contrib.auth.decorators import login_required
+
+
 
 def survey(request):
     return render(request, 'survey/survey.html')
