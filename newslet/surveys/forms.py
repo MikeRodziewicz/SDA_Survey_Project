c670298365206c1ef232755d95e1e8f2e9f44288
Damian <damianpud@gmail.com>
2020-11-28 16:01:25 +0100

    new survey model, add mixins plus random_users

--- newslet/surveys/forms.py
+++ newslet/surveys/forms.py
@@ -4,7 +4,7 @@ import re
 import math
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Layout, Submit, HTML
 
 from .models import Product, Company, Survey
 
@@ -56,22 +56,40 @@ class SurveyForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.fields['rate_1'].label = 'Jak oceniasz jakosc produktu?'
+        self.fields['rate_2'].label = 'Jak oceniasz cene produktu?'
+        self.fields['rate_3'].label = 'Ocen jaka wartosc ma dla Ciebie produkt?'
+        self.fields['rate_4'].label = 'Jak oceniasz nasza firme?'
+        self.fields['rate_5'].label = 'Jak oceniasz kontakt z klientem?'
+        self.fields['description'].label = 'Napisz nam coś o produkcie:'
         self.helper.layout = Layout(
-            'question',
-            'answer',
-            Submit('submit', 'Add Survey', css_class="btn-success"),
+            'company',
+            'product',
+            'rate_1',
+            'rate_2',
+            'rate_3',
+            'rate_4',
+            'rate_5',
+            'description',
+            Submit('submit', 'Add Survey', css_class="btn-success")
             )
 
     class Meta:
         model = Survey
         fields = '__all__'
 
-    question = forms.CharField()
-    answer = forms.CharField(widget=forms.Textarea, required=False)
+    company = forms.ModelChoiceField(queryset=Company.objects)
+    product = forms.ModelChoiceField(queryset=Product.objects)
+    rate_1 = forms.IntegerField(min_value=1, max_value=5)
+    rate_2 = forms.IntegerField(min_value=1, max_value=5)
+    rate_3 = forms.IntegerField(min_value=1, max_value=5)
+    rate_4 = forms.IntegerField(min_value=1, max_value=5)
+    rate_5 = forms.IntegerField(min_value=1, max_value=5)
+    description = forms.CharField(widget=forms.Textarea, required=False)
 
-    def clean_answer(self):
-        initial = self.cleaned_data['answer']
+    def clean_description(self):
+        initial = self.cleaned_data['description']
         sentences = re.sub(r'\s*\.\s*', '.', initial).split('.')
         cleaned = '. '.join(sentence.capitalize() for sentence in sentences)
-        self.cleaned_data['answer'] = cleaned
+        self.cleaned_data['description'] = cleaned
         return cleaned
--- newslet/surveys/models.py
+++ newslet/surveys/models.py
@@ -17,8 +17,14 @@ class Product(models.Model):
 
 
 class Survey(models.Model):
-    question = models.CharField(max_length=128)
-    answer = models.TextField(null=True, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    rate_1 = models.IntegerField()
+    rate_2 = models.IntegerField()
+    rate_3 = models.IntegerField()
+    rate_4 = models.IntegerField()
+    rate_5 = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.question
